f8f4c6b646d9fbaa60d579df3bcea0f964c3543c
Lakhan Gupta <lakhan@destinationtech.net>
2026-03-16 13:24:58 +0530

    disable netlify angular plugin

--- obj/Debug/net10.0/ECommerce.Client.AssemblyInfo.cs
+++ obj/Debug/net10.0/ECommerce.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e32280fde557d92d202d90cce0d6469b5b2b196c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
